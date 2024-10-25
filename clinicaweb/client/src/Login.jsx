9505794ba80e7940788796995736390b4dcc63bb
Daniel <97762977+Daniel95URU@users.noreply.github.com>
2024-10-25 00:48:41 -0400

    Add files via upload

---
+++ clinicaweb/client/src/Login.jsx
@@ -0,0 +1,68 @@
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Login.css';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError(''); // Eliminar la marca del error anterior
+
+    try {
+      const response = await fetch('/api/login', { // Usar la ruta /api
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        if (data.success) {
+          navigate('/Inicio'); 
+        }
+      } else {
+        setError(data.error);
+      }
+    } catch (error) {
+      setError('Error de conexión');
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Usuario:</label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Contraseña:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
---
+++ clinicaweb/client/src/dashboard.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import Login from './Login';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>≡</h1>
+      <Login />
+    </div>
+  );
+};
+
+export default Dashboard;
